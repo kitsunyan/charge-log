82c261410882b3f5f23558d0e5a58717a3ebde62
kitsunyan <kitsunyan@airmail.cc>
2019-07-18 00:06:03 +0300

    Add systemd support and configure script

--- common.h
+++ common.h
@@ -1,7 +1,7 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define LOG_FILE "/run/charge-log"
+#define LOG_FILE RUNSTATEDIR "/charge-log"
 #define UPDATE_INTERVAL 60
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
